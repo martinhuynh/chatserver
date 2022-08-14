a438ca8ed7f054845da4978b27e68324be19e44c
martinhuynh <maritnhuynhp@gmail.com>
2022-08-14 00:26:52 -0700

    Small changes.

--- src/main/java/com/martin/chatserver/config/WebsocketConfig.java
+++ src/main/java/com/martin/chatserver/config/WebsocketConfig.java
@@ -5,13 +5,17 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").setAllowedOrigins("*");
+        RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").setAllowedOrigins("*").setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy));
     }
 
     @Override
